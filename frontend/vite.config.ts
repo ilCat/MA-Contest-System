ed3262a1387c9f45857bdae035ad0e69a2859e62
AlexisSilva <alexisnathaniel.s@gmail.com>
2024-05-04 18:11:56 -0300

    add details

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  preview: {
-   port: 8080,
-   strictPort: true,
-  },
-  server: {
-   port: 8080,
-   strictPort: true,
-   host: true,
-   origin: "http://0.0.0.0:8080",
-  },
+  // preview: {
+  //  port: 5173,
+  //  strictPort: true,
+  // },
+  // server: {
+  //  port: 5173,
+  //  strictPort: true,
+  //  host: true,
+  //  origin: "http://0.0.0.0:5173",
+  // },
 })
